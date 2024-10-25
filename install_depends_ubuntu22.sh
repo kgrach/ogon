8ff906b8763da1f9046947fb4c1d63b4f0243efb
Kirill Grachev <pllayer@mail.ru>
2024-10-25 14:15:35 +0700

    Upd scripts

--- install_depends_ubuntu22.sh
+++ install_depends_ubuntu22.sh
@@ -1,3 +1,3 @@
-sudo apt install libprotobuf-dev libprotoc-dev protobuf-compiler protobuf-c-compiler \
+sudo apt install -y libprotobuf-dev libprotoc-dev protobuf-compiler protobuf-c-compiler \
   		 libpam0g-dev libboost-dev libdbus-1-dev automake libpam-systemd ca-certificates \
   		 libprotobuf-c-dev ssl-cert
