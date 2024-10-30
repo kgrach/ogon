a65dd5423dacb2345ad224ce434d51293906f8b8
Kirill Grachev <pllayer@mail.ru>
2024-10-30 15:02:34 +0700

    Upd script 4 redos

--- install-services.sh
+++ install-services.sh
@@ -1,4 +1,4 @@
-cp misc/config_ubuntu/config.ini /opt/ogon/etc/ogon/config.ini
+cp misc/config/config.ini.debian /opt/ogon/etc/ogon/config.ini
 sudo ln -s /opt/ogon/etc/dbus-1/system.d/ogon.SessionManager.conf /etc/dbus-1/system.d/
 sudo service dbus reload
 sudo cp misc/pam.d/ogon.debian /etc/pam.d/ogon
@@ -6,5 +6,3 @@ sudo cp misc/ogon-rdp-server.service /etc/systemd/system
 sudo cp misc/ogon-session-manager.service /etc/systemd/system
 sudo systemctl daemon-reload
 mkdir -p /opt/ogon/var/run
-sudo systemctl enable ogon-rdp-server
-sudo systemctl start ogon-rdp-server
